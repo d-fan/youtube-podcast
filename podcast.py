1bd6152360494d62fba1732f14e9568464b6929f
David Fan <ictoab@gmail.com>
2016-09-29 22:50:49 -0700

    update downloader

--- podcast.py
+++ podcast.py
@@ -4,33 +4,62 @@ from apiclient.discovery import build
 import json
 import pprint
 import pafy
+import re
+import yaml
 
-class YoutubePodcast:
-    def __init__(self, api_key):
-        self.service = build('youtube', 'v3', developerKey=api_key)
+config = {}
 
-    def GetChannelIdFromChannelName(self, username):
-        request = self.service.channels().list(part="id", forUsername=username)
-        response = request.execute()
-        if len(response['items']) != 1:
-            return ''
-        return response['items'][0]['id']
+class Channel:
+    def __init__(self, name):
+        self.service = build('youtube', 'v3', developerKey=config['key'])
+        self.name = name
+        self.id = None
+        self.videos = []
+        self.ready = False
 
-    def GetVideosForChannel(self, channel_id, maxResults=25):
-        request = self.service.search().list(order='date', part='id', channelId=channel_id, maxResults=maxResults)
-        response = request.execute()
-        videos = []
+    def load(self):
+        # Get id
+        kwargs = {
+            'part': 'id',
+            'forUsername': self.name
+        }
+        response = self.service.channels().list(**kwargs).execute()
+        if len(response['items']) == 1:
+            self.id = response['items'][0]['id']
+        else:
+            print "Error: Could not get id for channel %s" % self.name
+            return
+
+        # Get videos
+        kwargs = {
+            'order': 'date',
+            'part': 'id',
+            'channelId': self.id,
+            'maxResults': 25,
+        }
+        response = self.service.search().list(**kwargs).execute()
         for item in response['items']:
+            pprint.pprint(item)
             if item['id']['kind'] == u'youtube#video':
-                videos.append(item['id']['videoId'])
-        return videos
+                self.videos.append(item['id']['videoId'])
+        return self
 
 
+def LoadConfig(file_name):
+    global config
+    with open(file_name, "r") as input_file:
+        config = yaml.load(input_file)
+        print "Config load successful"
+        return
+    print "Failed to load config"
+    exit()
+    
 if __name__ == "__main__":
-    podcast = YoutubePodcast('AIzaSyClwOKX0tQJsHdchbSjvetfWYgAazIJD8U')
-    channel_id = podcast.GetChannelIdFromChannelName('BrookingsInstitution')
-    videos = podcast.GetVideosForChannel(channel_id)
-    video = pafy.new(videos[0])
-    print video
-    print video.description.encode('utf-8')
-    print video.getbestaudio()
+    LoadConfig("config.yml")
+    for channel_name in config['channels']:
+        channel = Channel(channel_name)
+        channel.load()
+        for video_id in channel.videos:
+            path = "media/%s/%s" % (channel_name, video_id)
+            video = pafy.new(video_id)
+            video.getbestaudio().download(filepath=path)
